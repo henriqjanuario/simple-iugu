cb6e635b41f8e7cccefad928277456e16f790787
riquejanuario <riquejanuario@jera.com.br>
2018-08-07 19:34:38 -0400

    realocate services

---
+++ lib/simple_iugu/services/base.rb
@@ -0,0 +1,53 @@
+require 'base64'
+require 'json'
+require 'byebug'
+require 'httparty'
+
+module SimpleIugu
+  module Services
+    class Base
+
+      def self.get(endpoint, query, account_access_token)
+        HTTParty.get("#{SimpleIugu.base_uri}/#{SimpleIugu.api_version}#{endpoint}", headers: set_headers(account_access_token), query: query)
+      end
+
+      def self.post(endpoint, body, account_access_token)
+        HTTParty.post("#{SimpleIugu.base_uri}/#{SimpleIugu.api_version}#{endpoint}", headers: set_headers(account_access_token), body: body)
+      end
+
+      def self.put(endpoint, body, account_access_token)
+        HTTParty.put("#{SimpleIugu.base_uri}/#{SimpleIugu.api_version}#{endpoint}", headers: set_headers(account_access_token), body: body)
+      end
+
+      def self.delete(endpoint, account_access_token)
+        HTTParty.delete("#{SimpleIugu.base_uri}/#{SimpleIugu.api_version}#{endpoint}", headers: set_headers(account_access_token))
+      end
+
+      def self.set_headers(account_access_token)
+        return { 'Content-Type': 'application/json', 'Authorization': "Basic #{Base64.encode64(ensure_account_access_token(account_access_token))}" }
+      end
+
+      def self.ensure_account_access_token(account_access_token)
+        if account_access_token.nil?
+          account_access_token = SimpleIugu.is_test ? SimpleIugu.api_key_test : SimpleIugu.api_key
+        else
+          account_access_token
+        end
+        # 'bebccae8ba2ed6e24c661929f845144f'
+      end
+
+      def self.symbolize(obj)
+        return obj.reduce({}) do |memo, (k, v)|
+          memo.tap { |m| m[k.to_sym] = symbolize(v) }
+        end if obj.is_a? Hash
+
+        return obj.reduce([]) do |memo, v|
+          memo << symbolize(v); memo
+        end if obj.is_a? Array
+
+        obj
+      end
+
+    end
+  end
+end
---
+++ lib/simple_iugu/services/payment_token.rb
@@ -0,0 +1,14 @@
+module SimpleIugu
+  module Services
+    class PaymentToken < Base
+
+      BASE_ENDPOINT = 'payment_token'
+
+      def self.create(body = {}, access_token = nil)
+        response = post(BASE_ENDPOINT, body, access_token)
+
+        symbolize(response.parsed_response)
+      end
+    end
+  end
+end
