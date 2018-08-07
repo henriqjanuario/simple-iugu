60f3677acdf23252dfcdab67ab74aa7ffe359987
riquejanuario <riquejanuario@jera.com.br>
2018-08-07 19:25:02 -0400

    Solve polimorfic relation

---
+++ lib/generators/active_record/templates/create_iugu_customers.rb
@@ -0,0 +1,24 @@
+class CreateIuguCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :iugu_customers do |t|
+
+      t.string :email
+      t.string :name
+      t.string :notes
+      t.integer :phone
+      t.integer :phone_prefix
+      t.string :cpf_cpnj
+      t.string :cc_emails
+      t.string :zipcode
+      t.string :number
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :district
+      t.string :complement
+      t.references :customerable, polymorphic: true, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
--- lib/simple_iugu/models/customer.rb
+++ lib/simple_iugu/models/customer.rb
@@ -1,4 +1,4 @@
-class Iugu::Customer < ActiveRecord::Base
+class SimpleIugu::Customer < ActiveRecord::Base
   self.table_name = "iugu_customers"
 
   belongs_to :customerable, polymorphic: true
